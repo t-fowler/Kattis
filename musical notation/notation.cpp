6fb934bdd1e3a96ce33bf5a939b3456b191eff79
Tyler Fowler <tfowl@proton.me>
2022-06-29 09:34:15 -0700

    Simplified logic.

--- musical notation/notation.cpp
+++ musical notation/notation.cpp
@@ -1,5 +1,6 @@
 #include <array>
 #include <iostream>
+#include <map>
 #include <string>
 
 enum Note {
@@ -20,86 +21,26 @@ void fillLinesOtherThan(std::array<std::string, 14> &sheet_music, Note note) {
     }
 }
 
-void add_notation(std::array<std::string, 14> &sheet_music, int n) {
-    for (int i = 0; i < n; i++) {
-        char note;
-        int length;
-        std::cin >> note;
-        
-        if (std::cin.peek() == ' ') {
-            length = 1;
-        } else {
-            std::cin >> length;
-        }
-
-        for (int j = 0; j < length; j++) {
-             switch (note) {
-                case 'G':
-                    sheet_music[G].append("*");
-                    fillLinesOtherThan(sheet_music, G);
-                    break;
-                case 'F':
-                    sheet_music[F].append("*");
-                    fillLinesOtherThan(sheet_music, F);
-                    break;
-                case 'E':
-                    sheet_music[E].append("*");
-                    fillLinesOtherThan(sheet_music, E);
-                    break;
-                case 'D':
-                    sheet_music[D].append("*");
-                    fillLinesOtherThan(sheet_music, D);
-                    break;
-                case 'C':
-                    sheet_music[C].append("*");
-                    fillLinesOtherThan(sheet_music, C);
-                    break;
-                case 'B':
-                    sheet_music[B].append("*");
-                    fillLinesOtherThan(sheet_music, B);
-                    break;
-                case 'A':
-                    sheet_music[A].append("*");
-                    fillLinesOtherThan(sheet_music, A);
-                    break;
-                case 'g':
-                    sheet_music[g].append("*");
-                    fillLinesOtherThan(sheet_music, g);
-                    break;
-                case 'f':
-                    sheet_music[f].append("*");
-                    fillLinesOtherThan(sheet_music, f);
-                    break;
-                case 'e':
-                    sheet_music[e].append("*");
-                    fillLinesOtherThan(sheet_music, e);
-                    break;
-                case 'd':
-                    sheet_music[d].append("*");
-                    fillLinesOtherThan(sheet_music, d);
-                    break;
-                case 'c':
-                    sheet_music[c].append("*");
-                    fillLinesOtherThan(sheet_music, c);
-                    break;
-                case 'b':
-                    sheet_music[b].append("*");
-                    fillLinesOtherThan(sheet_music, b);
-                    break;
-                case 'a':
-                    sheet_music[a].append("*");
-                    fillLinesOtherThan(sheet_music, a);
-                    break;
-            }
-        }
-        if (i != n-1)
-            fillLinesOtherThan(sheet_music, NO_NOTE);
-    }
-}
-
 int main () {
     int n = 0;
     std::cin >> n;
+
+    std::map<char, Note> charToNoteMap = {
+        {'G', G},
+        {'F', F},
+        {'E', E},
+        {'D', D},
+        {'C', C},
+        {'B', B},
+        {'A', A},
+        {'g', g},
+        {'f', f},
+        {'e', e},
+        {'d', d},
+        {'c', c},
+        {'b', b},
+        {'a', a}
+    };
 
     std::array<std::string, 14> output = {
         "G: ",
@@ -118,7 +59,30 @@ int main () {
         "a: "
     };
 
-    add_notation(output, n);
+    for (int i = 0; i < n; i++) {
+        char note;
+        int length;
+        std::cin >> note;
+
+        if (std::cin.peek() == ' ') {
+            length = 1;
+        } else {
+            std::cin >> length;
+        }
+        std::cin >> std::ws;
+
+        std::cerr << "note: " << note << std::endl 
+            << "length: " << length << std::endl << std::endl;
+            
+        // Add stars for the current note.
+        for (int j = 0; j < length; j++) {
+            output[charToNoteMap[note]].append("*");
+            fillLinesOtherThan(output, charToNoteMap[note]);
+        }
+        if (i != n-1)
+            fillLinesOtherThan(output, NO_NOTE);
+        
+    }
 
     for (std::string line : output) {
         std::cout << line << std::endl;
