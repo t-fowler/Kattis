dd5834f7b762f751107f1117b72c97a66cb1ef64
t-fowler <52936210+t-fowler@users.noreply.github.com>
2022-07-01 23:13:26 -0700

    Medium: mandelbrot

--- Mandelbrot/mandelbrot.cpp
+++ Mandelbrot/mandelbrot.cpp
@@ -6,37 +6,26 @@
 // Determine whether the given complex number is in the mandelbrot set.
 //
 
-typedef std::complex<double> complex;
+typedef std::complex<long double> complex;
 
-complex complexSquared(complex c) {
-    return complex(
-            c.real() * c.real() - c.imag() * c.imag(),
-            2 * c.real() * c.imag());
-}
-
-double complexModulus(complex c) {
-    return std::sqrt(c.real() * c.real() +
-                    c.imag() * c.imag());
-}
-
-bool mandelbrot(complex accumulator, complex c, int maxIterations) {
-    complex zN = complexSquared(accumulator) + c;
-    if (complexModulus(zN) > 2.0)
+bool mandelbrot(complex zN, complex c, int maxIterations) {
+    if (std::norm(zN) > 4.0)
         return false;
     if (maxIterations == 0)
         return true;
-    return mandelbrot(zN, c, maxIterations - 1);
+    return mandelbrot(zN * zN + c, c, maxIterations - 1);
 }
 
 int main() {
     for (int i = 0; i < 15; ++i) {
         if ((std::cin >> std::ws).eof()) {
+            std::cerr << "end of file." << std::endl;
             break;
         } else {
             double realPart, imaginaryPart;
             int maxIterations;
             std::cin >> realPart >> imaginaryPart >> maxIterations;
-            if (mandelbrot(0, complex(realPart, imaginaryPart), maxIterations - 1)) {
+            if (mandelbrot(0, complex(realPart, imaginaryPart), maxIterations)) {
                 std::cout << "Case " << i+1 << ": IN" << std::endl;
             } else {
                 std::cout << "Case " << i+1 << ": OUT" << std::endl;
