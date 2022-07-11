9a3ba10bcbfe47b321018564c99832213e06ca1e
Tyler Fowler <tfowl@proton.me>
2022-07-10 21:22:18 -0700

    Progress. Need to reduce time complexity

--- forest fruits/forest-fruits.cpp
+++ forest fruits/forest-fruits.cpp
@@ -9,19 +9,46 @@ int main() {
     int V, E, C, K, M;
     std::cin >> V >> E >> C >> K >> M >> std::ws;
 
+    int numberOfDays = M;
+    if (K < M) {
+        numberOfDays = K;
+    }
+
     std::vector<Edge> edgeList = readEdges(E);
-    std::vector<Vertex> fruitClearings = readFruitList(C);
-    std::map<int, int> fruitTimer = initTimer(V, fruitClearings);
+    std::vector<int> fruitClearings = readFruitList(C);
     std::map<int, int> distanceToFruit = findShortestPaths(V, edgeList);
+    
+    auto compareDistance = [&](int a, int b) {
+        if (distanceToFruit[a] == distanceToFruit[b]) return false;
+        if (distanceToFruit[a] == INT_MAX) return false;
+        if (distanceToFruit[b] == INT_MAX) return true;
+        return distanceToFruit[b] - distanceToFruit[a] > 0;
+    };
+    std::sort(fruitClearings.begin(), fruitClearings.end(), compareDistance);
+
+/*
+    std::cerr << "Fruit clearings ordered by distance: ";
+    for (auto i : fruitClearings) {
+        std::cerr << i << " ";
+    }
+    std::cerr << std::endl;
 
     for (int i = 1; i <= V; ++i) {
         std::cerr << "Distance to " << i << " is " 
             << distanceToFruit[i] << "." << std::endl;
     }
+*/
 
-    for (int i = 0; i <= K; ++i) {
-        
+
+
+    if (fruitClearings.size() < numberOfDays || fruitClearings[numberOfDays-1] == INT_MAX) {
+        std::cout << -1 << std::endl;
+    } else {
+        //std::cerr << "Kth shortest distance: " << fruitClearings[numberOfDays-1] << std::endl;
+        std::cout << distanceToFruit[fruitClearings[numberOfDays-1]]*2 << std::endl;
     }
+
+    return 0;
 }
 
 /**
@@ -45,39 +72,16 @@ std::vector<Edge> readEdges(int E) {
  * @brief Reads all the indices for clearings that grow fruit from standard input.
  * 
  * @param C The number of clearings that grow fruit.
- * @return std::vector<Vertex> The list of clearings.
+ * @return std::vector<int> The list of clearings.
  */
-std::vector<Vertex> readFruitList(int C) {
-    std::vector<Vertex> fruitClearings;
+std::vector<int> readFruitList(int C) {
+    std::vector<int> fruitClearings;
     for (int i = 0; i < C; ++i) {
         int v;
         std::cin >> v;
         fruitClearings.push_back(v);
     }
     return fruitClearings;
-}
-
-/**
- * @brief Provides a timer for tracking fruit growth in each clearing.
- * 
- * @param V The number of clearings.
- * @param fruitClearings A vector containing all the clearings that grow fruit.
- * 
- * @return std::map<int, int> A map from clearing index to an integer representing
- * the number of days until fruit grows in the clearing. If the value is negative,
- * then the clearing does not grow fruit. If it is zero, then the clearing contains
- * fruit. If positive, then the clearing will grow fruit in that number of days.
- * *Positive values must be decremented each day*.
- */
-std::map<int, int> initTimer(int V, std::vector<Vertex> fruitClearings) {
-    std::map<int, int> fruitTimer;
-    for (int i = 1; i <= V; ++i) {
-        fruitTimer[i] = -1;
-    }
-    for (Vertex v : fruitClearings) {
-        fruitTimer[v.index()] = 0;
-    }
-    return fruitTimer;
 }
 
 /**
@@ -88,7 +92,7 @@ std::map<int, int> initTimer(int V, std::vector<Vertex> fruitClearings) {
  * @return std::map<int, int> a map from clearing indices to the distance from the
  * cottage clearing.
  */
-std::map<int, int> findShortestPaths(int V, std::vector<Edge> edgeList) {
+std::map<int, int> findShortestPaths(int V, std::vector<Edge> &edgeList) {
     std::vector<int> visited = {1};
     std::map<int, int> distanceTo;
     for (int i = 1; i <= V; ++i) {
@@ -98,18 +102,16 @@ std::map<int, int> findShortestPaths(int V, std::vector<Edge> edgeList) {
 
     auto compareDistance = [&](int a, int b) {
         if (distanceTo[a] == distanceTo[b]) return 0;
-        if (distanceTo[a] == -1) return 1;
-        if (distanceTo[b] == -1) return -1;
+        if (distanceTo[a] == INT_MAX) return 1;
+        if (distanceTo[b] == INT_MAX) return -1;
         return distanceTo[a] - distanceTo[b];
     };
-    std::vector<int> queue;
-    queue.push_back(1);
-    std::push_heap(queue.begin(), queue.end(), compareDistance);
+    std::priority_queue<int, std::vector<int>, decltype(compareDistance)> queue(compareDistance);
+    queue.push(1);
 
     while (!queue.empty()) {
-        std::pop_heap(queue.begin(), queue.end(), compareDistance);
-        int node = queue.back();
-        queue.pop_back();
+        int node = queue.top();
+        queue.pop();
         
 
         for (Edge e : edgeList) {
@@ -118,16 +120,7 @@ std::map<int, int> findShortestPaths(int V, std::vector<Edge> edgeList) {
             int nextNode = e.toNode();
             if (distanceTo[nextNode] > distanceTo[node] + e.weight()) {
                 distanceTo[nextNode] = distanceTo[node] + e.weight();
-                if (std::find(queue.begin(), queue.end(), nextNode)
-                    == queue.end()) {
-                    queue.push_back(nextNode);
-                    std::push_heap(queue.begin(), queue.end(), compareDistance);
-                } else {
-                    std::remove(queue.begin(), queue.end(), nextNode);
-                    std::make_heap(queue.begin(), queue.end(), compareDistance);
-                    queue.push_back(nextNode);
-                    std::push_heap(queue.begin(), queue.end(), compareDistance);
-                }
+                queue.push(nextNode);
             }
         }
     }
--- forest fruits/forest-fruits.hpp
+++ forest fruits/forest-fruits.hpp
@@ -4,6 +4,5 @@
 #include "ForestGraph.hpp"
 
 std::vector<Edge> readEdges(int E);
-std::vector<Vertex> readFruitList(int C);
-std::map<int, int> initTimer(int V, std::vector<Vertex> fruitClearings);
-std::map<int, int> findShortestPaths(int V, std::vector<Edge> edgeList);
+std::vector<int> readFruitList(int C);
+std::map<int, int> findShortestPaths(int V, std::vector<Edge> &edgeList);
